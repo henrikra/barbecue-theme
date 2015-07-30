57b198f13c41f7e5a8c1dd696838dc5b2148d6a0
Henrik Raitasola <heka.fof@gmail.com>
2015-07-30 21:44:18 +0300

    Lisätty staattinen ruokapöydän varaussivu

---
+++ page-reservation.php
@@ -0,0 +1,20 @@
+<?php
+/*
+	Template Name: Reservation Page
+*/
+
+
+get_header(); ?>
+
+
+<header class="small-header">
+	<h1 class="small-header--title">Varaa pöytä</h1>
+</header>
+
+<?php get_template_part('template-parts/content', 'table-reservation'); ?>
+
+<?php get_template_part('template-parts/content', 'map'); ?>
+
+<?php get_template_part('template-parts/content', 'contact'); ?>
+
+<?php get_footer(); ?>
---
+++ template-parts/content-table-reservation.php
@@ -0,0 +1,54 @@
+<section class="reservation">
+	<div class="container">
+		<h2 class="reservation--title">lorem ipsum dolor sit</h2>
+		<div class="row">
+			<div class="col-sm-10 col-sm-offset-1">
+				<p class="reservation--description">Consectetur adipisicing elit, sed do +358 400 637916 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in </p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-10 col-sm-offset-1">
+				<div class="panel panel-default reservation--form-panel">
+				  <div class="panel-body reservation--form-body">
+				  	<div class="row">
+				  		<div class="col-sm-6">
+				  			<div class="form-group">
+				  				<label for="first-name">Etunimi <span class="required">*</span></label>
+				  				<input id="first-name" type="text" class="form-control">
+				  			</div>
+				  			<div class="form-group">
+				  				<label for="date">Päivämäärä <span class="required">*</span></label>
+				  				<input id="date" type="text" class="form-control">
+				  			</div>
+				  			<div class="form-group">
+				  				<label for="phone">Puhelinnumero <span class="required">*</span></label>
+				  				<input id="phone" type="text" class="form-control">
+				  			</div>
+				  		</div>
+				  		<div class="col-sm-6">
+				  			<div class="form-group">
+				  				<label for="last-name">Sukunimi <span class="required">*</span></label>
+				  				<input id="last-name" type="text" class="form-control">
+				  			</div>
+				  			<div class="form-group">
+				  				<label for="time">Kellonaika <span class="required">*</span></label>
+				  				<input id="time" type="text" class="form-control">
+				  			</div>
+				  			<div class="form-group">
+				  				<label for="email">Sähköposti</label>
+				  				<input id="email" type="email" class="form-control">
+				  			</div>
+				  		</div>
+				  	</div>
+				  	<div class="form-group">
+		  				<label for="optional-info">Lisätiedot</label>
+		  				<textarea id="optional-info" class="form-control" rows="3"></textarea>
+		  			</div>
+				  </div>
+				</div>
+			</div>
+		</div>
+		<button type="button" class="btn btn-default cta-btn">Varaa pöytä</button>
+		
+	</div>
+</section>
